9b225cd6be1ed6731d8fd0f25f8f89e610fa85ba
Wolfgang Welz <welzwo@gmail.com>
2025-08-14 11:50:47 -0400

    WEB3-524: op-steel: use op-contracts v4.0.0 (#4)
    * use anchorStateRegistry to check for valid dispute games
    
    * use op-contracts v4.0.0
    
    * use current steel version
    
    * use steel contracts
    
    * update license header

--- examples/erc20-counter/contracts/src/Counter.sol
+++ examples/erc20-counter/contracts/src/Counter.sol
@@ -1,4 +1,4 @@
-// Copyright 2024 RISC Zero, Inc.
+// Copyright 2025 RISC Zero, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 pragma solidity ^0.8.20;
 
 import {IRiscZeroVerifier} from "risc0/IRiscZeroVerifier.sol";
-import {Steel} from "risc0/steel/Steel.sol";
+import {Steel} from "steel/Steel.sol";
 import {ICounter} from "./ICounter.sol";
 import {ImageID} from "./ImageID.sol"; // auto-generated contract after running `cargo build`.
 
