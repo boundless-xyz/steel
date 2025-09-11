f4414191e0d42993cda0ac1a7f394252b7e32a9b
Wolfgang Welz <welzwo@gmail.com>
2025-09-11 21:54:25 +0200

    do not export Error

--- crates/steel/src/history/mod.rs
+++ crates/steel/src/history/mod.rs
@@ -18,7 +18,7 @@ use alloy_primitives::{Sealed, B256, U256};
 use beacon::{BeaconCommit, GeneralizedBeaconCommit, STATE_ROOT_LEAF_INDEX};
 use beacon_roots::BeaconRootsContract;
 use serde::{Deserialize, Serialize};
-use state::SingleContractState;
+use state::{Error, SingleContractState};
 
 use crate::{
     beacon, beacon::BeaconBlockId, BlockHeaderCommit, Commitment, CommitmentVersion, ComposeInput,
@@ -29,7 +29,6 @@ mod eip2935;
 mod state;
 
 pub use eip2935::{HistoryCommit as Eip2935HistoryCommit, HistoryInput as Eip2935HistoryInput};
-pub use state::Error;
 
 /// Input recursively committing to multiple Beacon Chain block roots.
 pub type HistoryInput<F> = ComposeInput<F, HistoryCommit>;
