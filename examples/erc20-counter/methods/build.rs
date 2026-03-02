47694a2dc810f514465a521aee52d68cdac5f2f5
Wolfgang Welz <welzwo@gmail.com>
2026-03-02 21:50:20 +0100

    update build script

--- examples/erc20-counter/methods/build.rs
+++ examples/erc20-counter/methods/build.rs
@@ -14,7 +14,7 @@
 
 use std::{collections::HashMap, env, path::PathBuf};
 
-use risc0_build::{embed_methods_with_options, DockerOptionsBuilder, GuestOptionsBuilder};
+use risc0_build::{DockerOptionsBuilder, GuestOptionsBuilder, embed_methods_with_options};
 use risc0_build_ethereum::generate_solidity_files;
 
 // Paths where the generated Solidity files will be written.
@@ -26,11 +26,13 @@ fn main() {
     // guest. Check the RISC0_USE_DOCKER variable and use Docker to build the guest if set.
     println!("cargo:rerun-if-env-changed=RISC0_USE_DOCKER");
     println!("cargo:rerun-if-changed=build.rs");
-    let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+
     let mut builder = GuestOptionsBuilder::default();
     if env::var("RISC0_USE_DOCKER").is_ok() {
+        let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+        let root_dir = manifest_dir.join("..").join("..").join("..");
         let docker_options = DockerOptionsBuilder::default()
-            .root_dir(manifest_dir.join(".."))
+            .root_dir(root_dir)
             .build()
             .unwrap();
         builder.use_docker(docker_options);
@@ -39,12 +41,14 @@ fn main() {
 
     // Generate Rust source files for the methods crate.
     let guests =
-        embed_methods_with_options(HashMap::from([("erc20-counter-guests", guest_options)]));
+        embed_methods_with_options(HashMap::from([("erc20-counter-guest", guest_options)]));
 
     // Generate Solidity source files for use with Forge.
     let solidity_opts = risc0_build_ethereum::Options::default()
         .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
         .with_elf_sol_path(SOLIDITY_ELF_PATH);
 
-    generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
+    if let Err(e) = generate_solidity_files(guests.as_slice(), &solidity_opts) {
+        println!("cargo:warning=Failed to generate Solidity files: {e}");
+    };
 }
